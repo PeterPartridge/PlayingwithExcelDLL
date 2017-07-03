ce7ec452d5244640b7384874880247e02e32179d
Peter Partridge <petertoby@aol.com>
2017-07-03 23:00:41 +0100

    Adding filers
    still need to learn vs uploads

---
+++ EcxelTest/EcxelTest/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Office.Interop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace EcxelTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            play();
+        }
+        public void play()
+        {
+            List<MyItems> items = new List<MyItems>();
+            Excel.Application exclApp = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook wkbook = exclApp.Workbooks.Open("D:/Coding/excelTest.xlsx");
+            Excel._Worksheet wkSheet = wkbook.Worksheets[1];
+            Excel.Range usedRange = wkSheet.UsedRange;
+            
+            int rowCounter = usedRange.Rows.Count;
+            int colCounter = usedRange.Columns.Count;
+
+            int rowCount = 2;
+            int colCount = 1;
+
+            while (rowCount < rowCounter)
+            {
+               
+                    items.Add(new MyItems { Thingy = usedRange.Cells[rowCount, colCount].Value2.ToString(), Thingy2 = usedRange.Cells[rowCount + 1, colCount + 1].Value2.ToString() });
+                   
+                
+                
+                rowCount++;
+
+            }
+
+
+            wkbook.Close();
+            exclApp.Quit();
+
+
+
+        }
+
+
+        public class MyItems
+        {
+
+            public string Thingy { get; set; }
+            public string Thingy2 { get; set; }
+
+
+        }
+
+
+    }
+}
+
